c2ba41ade6a64fc3d2c51cd8a5479f00b33685fb
brazalc <alcidesbraz@hotmail.com>
2024-03-17 00:38:38 -0300

    filtrando modulos

--- main.tf
+++ main.tf
@@ -98,7 +98,7 @@ resource "aws_subnet" "application-eks-1" {
 
 resource "aws_subnet" "application-eks-2" {
   vpc_id     = aws_vpc.application-eks.id
-  cidr_block = "10.0.1.0/24"
+  cidr_block = "10.0.2.0/24"
   availability_zone = "us-east-1a"
 }
 
